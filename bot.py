50bf485a006f2e52948e7d39923f74a5fe960e56
Ariel-Alejandr0 <65261302+Ariel-Alejandr0@users.noreply.github.com>
2021-01-24 14:05:37 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,15 @@ from os import linesep #serve para dar quebra de linha;
 #link de previsão do tempo que será enviado;
 link = "https://www.google.com/search?q=previs%C3%A3o+do+tempo&oq=previs%C3%A3o+do&aqs=chrome.2.69i57j69i59l2j69i60l2.8463j0j1&sourceid=chrome&ie=UTF-8"
 
-token = '***COLE_O_SEU_TOKEN_AQUI***'#token do bot;
+token = '***COLOQUE_O_SEU_TOKEN_AQUI***'#token do bot;
 
 bot = telepot.Bot(token) #passando o token para poder gerenciar o bot;
 
 cont = 0 #contador, serve para impedir envio excessivo de mensagens;
 
 mensagemEnviada = False # variável para verificar o envio da mensagem;
+
+
 
 OldData = 0 # variável qua armazena  a antiga data;
 
@@ -21,23 +23,34 @@ while True: # Loop infinito para bot nunca parar.
     
                                          #pega o ultimo elemento da variável
     EditAtualizacoes = atualizacaoes[-1] #neste caso serve apenas para simplificar,
-                                         #a variável anterior;    
-                  
-    DATA = int(EditAtualizacoes['message']['date']) #pega o ID da mensagem;
-    CHAT_ID = int(EditAtualizacoes['message']['chat']['id']) #pega o ID do chat;
-    NOME = str(EditAtualizacoes['message']['from']['first_name']) # pega o nome de quem enviou a msg; 
-    PegarMensagens = str(EditAtualizacoes['message']['text']) #pega a mensagem enviada;
+                                         #a variável anterior;
+    
+    class DADOS: # classe DADOS
+        def __init__(self, msg): # função que cria todas as características da classe
+            self.DATA = int(EditAtualizacoes[msg]['date']) #pega o ID da mensagem;
+            
+            self.CHAT_ID = int(EditAtualizacoes[msg]['chat']['id']) #pega o ID do chat;
+            self.NOME = str(EditAtualizacoes[msg]['from']['first_name']) # pega o nome de quem enviou a msg; 
+            self.PegarMensagens = str(EditAtualizacoes[msg]['text']) #pega a mensagem enviada;
 
-    if DATA != OldData: # verifica se existe diferença entre as atualizações de msg; 
+    try:
+        DADOS1 = DADOS('message')
+    
+    except(KeyError):
+        DADOS1 = DADOS('edited_message')
+
+
+    if DADOS1.DATA != OldData: # verifica se existe diferença entre as atualizações de msg; 
         mensagemEnviada = False # Mensagem ainda não foi envida;
 
-    OldData = DATA
+    OldData = DADOS1.DATA
 
     if mensagemEnviada == False: #se mensagem não foi enviada; 
-        if PegarMensagens == "/tempo": #verifica se a mensagem é igual a /tempo;
-            bot.sendMessage(CHAT_ID,f'Olá {NOME} aqui está o link: {linesep}{link}') #envia o link para o seguinte chat ID;
-            print(PegarMensagens)
-            print(NOME)
+        if DADOS1.PegarMensagens == "/tempo": #verifica se a mensagem é igual a /tempo;
+            bot.sendMessage(DADOS1.CHAT_ID,f'Olá {DADOS1.NOME} aqui está o link: {linesep}{link}') #envia o link para o seguinte chat ID;
+            print(DADOS1.PegarMensagens)
+            print(DADOS1.NOME)
             mensagemEnviada = True # mensagem foi enviada;
 
     sleep(0.1) #pausa de 1 segundo;
+
