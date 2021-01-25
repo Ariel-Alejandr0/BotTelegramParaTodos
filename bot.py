5448d25dbfc8caf0c1822259c44d90a91de5287c
Ariel-Alejandr0 <65261302+Ariel-Alejandr0@users.noreply.github.com>
2021-01-25 00:07:04 -0300

    Resolvendo Bug do sticker

--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@ from os import linesep #serve para dar quebra de linha;
 #link de previsão do tempo que será enviado;
 link = "https://www.google.com/search?q=previs%C3%A3o+do+tempo&oq=previs%C3%A3o+do&aqs=chrome.2.69i57j69i59l2j69i60l2.8463j0j1&sourceid=chrome&ie=UTF-8"
 
-token = 'COLOQUE_SEU_TOKEN_AQUI'#token do bot;
+token = '1572619085:AAEgPMqcm0MXOzQIBWo7z7EGEIyE_f_Iu8s'#token do bot;
 
 bot = telepot.Bot(token) #passando o token para poder gerenciar o bot;
 
+cont = 0 #contador, serve para impedir envio excessivo de mensagens;
+
 mensagemEnviada = False # variável para verificar o envio da mensagem;
 
-OldData = 0 # variável qua armazena  a antiga data;
+
+
+old_data = 0 # variável qua armazena  a antiga data;
+
+print("iniciou!!!")
 
 while True: # Loop infinito para bot nunca parar.
 
@@ -21,32 +27,37 @@ while True: # Loop infinito para bot nunca parar.
     EditAtualizacoes = atualizacaoes[-1] #neste caso serve apenas para simplificar,
                                          #a variável anterior;
     
-    class DADOS: # classe DADOS
-        def __init__(self, msg): # função que cria todas as características da classe
-            self.DATA = int(EditAtualizacoes[msg]['date']) #pega o ID da mensagem;
+    class Dados: # classe DADOS
+        def __init__(self, msg, IS_texto): # função que cria todas as características da classe
+            self.data = int(EditAtualizacoes[msg]['date']) #pega o ID da mensagem;
             
-            self.CHAT_ID = int(EditAtualizacoes[msg]['chat']['id']) #pega o ID do chat;
-            self.NOME = str(EditAtualizacoes[msg]['from']['first_name']) # pega o nome de quem enviou a msg; 
-            self.PegarMensagens = str(EditAtualizacoes[msg]['text']) #pega a mensagem enviada;
+            self.chat_id = int(EditAtualizacoes[msg]['chat']['id']) #pega o ID do chat;
+            self.nome = str(EditAtualizacoes[msg]['from']['first_name']) # pega o nome de quem enviou a msg; 
+            self.PegarMensagens = str(EditAtualizacoes[msg][IS_texto]) #pega a mensagem enviada;
 
     try:
-        DADOS1 = DADOS('message') # DADOS1 recebe a classe DADOS sendo que o parametro msg se torna 'message';
+        dadosFormatados = Dados('message', 'text') # dadosFormatados recebe a classe DADOS sendo que o parametro msg se torna 'message';
     
     except(KeyError): # tratamento de exceção
-        DADOS1 = DADOS('edited_message') # caso uma mensagem sofra uma edição e cause um KeyError,
+        dadosFormatados = Dados('message', 'sticker') # caso uma mensagem sofra uma edição e cause um KeyError,
                                          # o parametro msg deixa de ser 'message' e se torna 'edited_message';
+    except(KeyError):
+        dadosFormatados = Dados('edited_message', 'text')
+    
+    except(KeyError):
+        dadosFormatados = Dados('edited_message', 'sticker')
 
-
-    if DADOS1.DATA != OldData: # verifica se existe diferença entre as atualizações de msg; 
+    if dadosFormatados.data != old_data: # verifica se existe diferença entre as atualizações de msg; 
         mensagemEnviada = False # Mensagem ainda não foi envida;
-
-    OldData = DADOS1.DATA
+    else:
+        mensagemEnviada = True
+    old_data = dadosFormatados.data
 
     if mensagemEnviada == False: #se mensagem não foi enviada; 
-        if DADOS1.PegarMensagens == "/tempo": #verifica se a mensagem é igual a /tempo;
-            bot.sendMessage(DADOS1.CHAT_ID,f'Olá {DADOS1.NOME} aqui está o link: {linesep}{link}') #envia o link para o seguinte chat ID;
-            print(DADOS1.PegarMensagens) # ecreve no meu terminal /tempo
-            print(DADOS1.NOME) # mostra o nome do infeliz que enviou a mensagem;
+        if dadosFormatados.PegarMensagens == "/tempo": #verifica se a mensagem é igual a /tempo;
+            bot.sendMessage(dadosFormatados.chat_id,f'Olá {dadosFormatados.nome} aqui está o link: {linesep}{link}') #envia o link para o seguinte chat ID;
+            print(dadosFormatados.PegarMensagens) # ecreve no meu terminal /tempo
+            print(dadosFormatados.nome) # mostra o nome do infeliz que enviou a mensagem;
             mensagemEnviada = True # mensagem foi enviada;
 
     sleep(0.1) #pausa de 1 segundo;
